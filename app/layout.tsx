8a6f1d5f13f73c8b3f4b83b277d2d0c854886a86
apescasio <pescasioaaron92@gmail.com>
2025-11-28 09:47:17 +0100

    Static Export => GitHub Repo + CloudFlare Worker

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const inter = Inter({
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+      <body className="flex min-h-screen flex-col">
+        <RootProvider search={{ options: { type: 'static' } }}>{children}</RootProvider>
       </body>
     </html>
   );
