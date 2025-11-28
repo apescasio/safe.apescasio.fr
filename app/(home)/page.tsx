34940e1077f2410b65b92605c53bb0b832528727
apescasio <pescasioaaron92@gmail.com>
2025-11-28 09:39:26 +0100

    Updated Root page + Layout icons

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,16 +1,66 @@
-import Link from 'next/link';
+import ToDocsBtn from '@/components/to-docs-btn';
+import { DOCS } from '@/consts';
+import IconFour from '@/components/icons/four';
 
 export default function HomePage() {
+  const title = "Formulate, Organize, Unite, Reflect.";
+
+  const paragraph = (
+    <>
+      With the limited time, that we have on this Earth, I plan on leaving, a few things behind.<br />
+      - AIM. FOUR. SAFE. FUTURE.
+    </>
+  );
+
+  const description = (
+    <>
+      <a href={`/docs/home`}>cd /docs</a>
+      <br /><br />
+      <span className="text-sm">{paragraph}</span>
+    </>
+  );
+
   return (
-    <div className="flex flex-col justify-center text-center flex-1">
-      <h1 className="text-2xl font-bold mb-4">Hello World</h1>
-      <p>
-        You can open{' '}
-        <Link href="/docs" className="font-medium underline">
-          /docs
-        </Link>{' '}
-        and see the documentation.
-      </p>
-    </div>
+    <>
+      <main className="container flex flex-col gap-8 py-8 lg:gap-10 lg:py-10 max-w-7xl">
+        {/* Hero */}
+        <div className="flex flex-col items-center text-center">
+          <div className="bg-fd-card mb-4 aspect-square rounded-full border p-6 lg:p-6">
+            <IconFour className="size-36 shrink-0 lg:size-32 icon-adjust-memo -ml-1" aria-label="safe.apescasio.fr" />
+          </div>
+          <h1 className="text-2xl font-semibold lg:text-4xl">{title}</h1>
+          <p className="text-fd-muted-foreground lg:text-lg">{description}</p>
+        </div>
+
+        {/* Docs */}
+        <div className="flex flex-col items-center gap-8">
+          <div className="grid max-w-4xl grid-cols-2 lg:grid-cols-4 gap-4 w-full justify-center">
+            {Object.values(DOCS).map(({ title, path, icon: Icon }) => (
+              <ToDocsBtn
+                key={title}
+                title={title}
+                href={path}
+                icon={<Icon className="size-6 shrink-0 lg:size-4" />}
+              />
+            ))}
+          </div>
+
+          <p
+            suppressHydrationWarning
+            className="text-fd-muted-foreground -mt-4 text-center text-sm lg:text-base"
+          >
+            © {new Date().getFullYear()} Aaron (Iso) Pescasio /{" "}
+            <a
+              href="https://apescasio.fr/"
+              target="_blank"
+              className="hover:text-fd-accent-foreground underline"
+            >
+              apescasio.fr
+            </a>
+          </p>
+        </div>
+
+      </main>
+    </>
   );
 }
--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,9 +1,76 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import IconLinkedin from "@/components/icons/linkedin";
+import IconYouTubeV2 from "@/components/icons/youtubev2";
+import IconInstagram from "@/components/icons/instagram";
+import IconGitHub from "@/components/icons/github";
+import IconFour from "@/components/icons/four";
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: 'My App',
+      title: (
+        <>
+          <IconFour
+            className="size-9 shrink-0"
+            aria-label="Four Icon"
+            width={44}
+            height={44}
+          />
+          Safe
+        </>
+      ),
+      url: `/`,
     },
+    links: [
+      {
+        type: "main",
+        text: "Aaron (Iso) Pescasio",
+        url: "https://apescasio.fr",
+      },
+      {
+        type: "icon",
+        text: "YouTube",
+        url: "https://youtube.com/@apescasio",
+        icon: (
+          <span style={{ transform: "scale(1.2)" }}>
+            <IconYouTubeV2 />
+          </span>
+        ),
+        external: true,
+      },
+      {
+        type: "icon",
+        text: "LinkedIn",
+        url: "https://www.linkedin.com/in/aaron-pescasio",
+        icon: (
+          <span style={{ marginLeft: "-7px", transform: "scale(1.3)" }}>
+            <IconLinkedin />
+          </span>
+        ),
+        external: true,
+      },
+      {
+        type: "icon",
+        text: "Instagram",
+        url: "https://www.instagram.com/himapescasio",
+        icon: (
+          <span style={{ marginLeft: "-7px", transform: "scale(1.2)" }}>
+            <IconInstagram />
+          </span>
+        ),
+        external: true,
+      },
+      {
+        type: "icon",
+        text: "GitHub",
+        url: "https://github.com/apescasio",
+        icon: (
+          <span style={{ marginLeft: "-7px", transform: "scale(1.3)" }}>
+            <IconGitHub />
+          </span>
+        ),
+        external: true,
+      },
+    ],
   };
 }
