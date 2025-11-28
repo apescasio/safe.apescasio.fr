ccafa69547b581a5ef44800ab31059526282f4c3
apescasio <pescasioaaron92@gmail.com>
2025-11-28 09:36:26 +0100

    Added Components folder for icons + buttons docs

---
+++ components/icons/four.tsx
@@ -0,0 +1,23 @@
+import { SVGProps } from 'react';
+
+export default function IconFour(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 48 48"
+      {...props}
+    >
+      <g
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="4"
+      >
+        <path d="M26.977 34V14L18 26.997v2.023h12" />
+      </g>
+    </svg>
+  );
+}
