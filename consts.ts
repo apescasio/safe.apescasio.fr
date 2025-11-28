858031608eb0f2274af0f48e6edc8174d60f9cfe
apescasio <pescasioaaron92@gmail.com>
2025-11-28 09:37:13 +0100

    Added consts.ts for docs title + description

---
+++ consts.ts
@@ -0,0 +1,31 @@
+import IconHome from './components/icons/home';
+import IconAzure from './components/icons/azure';
+import IconIntune from './components/icons/intune';
+import IconPython from './components/icons/python';
+
+export const DOCS = {
+    home: {
+        title: "Home",
+        path: "/docs/home",
+        icon: IconHome,
+        desc: "Formulate, Organize, Unite, Reflect.",
+    },
+    azure: {
+        title: "Azure",
+        path: "/docs/azure",
+        icon: IconAzure,
+        desc: "The cloud platform of Microsoft offering a comprehensive range of services (200+) to design, deploy, and manage applications, infrastructures, and solutions, with flexibility, scalability, and security tailored to modern needs.",
+    },
+    intune: {
+        title: "Intune",
+        path: "/docs/intune",
+        icon: IconIntune,
+        desc: "A complete cloud solution from Microsoft that allows you to manage and secure your organization's devices and applications.",
+    },
+    python: {
+        title: "Python",
+        path: "/docs/python",
+        icon: IconPython,
+        desc: "A complete cloud solution from Microsoft that allows you to manage and secure your organization's devices and applications.",
+    },
+};
